f98ebdea00d73ef726cb64df6582bc3932510737
potdaraayush <potdaraayush2023@gmail.com>
2024-07-18 20:17:13 +0530

    understand git better/no relation w backend

---
+++ backend/dummy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function dummy() {
+  return (
+    <div>this is just for me to understand git. and has no relation w backend whatsoever</div>
+  )
+}
+
+export default dummy
